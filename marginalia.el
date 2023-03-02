6d69db6842d51298e56fe1ef7792e09e79679b03
Omar Antolín <omar.antolin@gmail.com>
2023-03-01 20:49:14 -0600

    where-is candidates are commands

--- marginalia.el
+++ marginalia.el
@@ -146,7 +146,8 @@ determine it."
   :type '(repeat (choice symbol regexp)))
 
 (defcustom marginalia-command-categories
-  '((imenu . imenu))
+  '((imenu . imenu)
+    (where-is . command))
   "Associate commands with a completion category."
   :type '(alist :key-type symbol :value-type symbol))
 
